5a7ac4e99031a958e0bb6e87ce9fddef9c5ae275
Khakhalin Andrey Vladimirovich <avkhakhalin@mail.ru>
2021-09-16 18:32:45 +0300

    Выполнение 1 пункта ТЗ.
    Вынесен API ключ во внешний файл. Создан класс "BuildConfig" через "build.gradle".
    Однако в классе "ConstantsRepository" оставлено поле с явным прописыванием ключа для того, чтобы вы могли его тестировать в себя.
    
    Ссылка на файл с репозиторием проекта:
    https://github.com/AVKhakhalin/YandexWeatherWorkFireFox

--- app/src/main/java/com/example/yandexweatherwork/repository/ConstantsRepository.kt
+++ app/src/main/java/com/example/yandexweatherwork/repository/ConstantsRepository.kt
@@ -1,5 +1,7 @@
 package com.example.yandexweatherwork.repository
 
+import com.example.yandexweatherwork.BuildConfig
+
 class ConstantsRepository {
     companion object {
         // Константы для получения данных о погоде
@@ -10,6 +12,8 @@ class ConstantsRepository {
         @JvmField
         val YANDEX_KEY_TITLE : String = "X-Yandex-API-Key"
         @JvmField
+//        val YANDEX_KEY_VALUE : String = BuildConfig.WEATHER_API_KEY
+        // Оставил этот ключ как есть, чтобы Вы могли тестировать проект
         val YANDEX_KEY_VALUE : String = "ebbee072-d212-420e-9f62-4d716b0499e9"
         @JvmField
         val LATITUDE_NAME : String = "lat"
